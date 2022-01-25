756502c051ca5d957da7c7af8963348675a4ddb8
cromero <cromero@amplify.com>
2022-01-25 13:43:17 -0500

    created container deploy

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     module: {
         rules: [{
@@ -6,11 +8,15 @@ module.exports = {
             use: {
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/preset-react','@babel/preset-env'],
+                    presets: ['@babel/preset-react', '@babel/preset-env'],
                     plugins: ['@babel/plugin-transform-runtime'],
                 }
             }
-            }
+        }
         ]
-    }
-}
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './public/index.html'
+    }),
+    ]
+};

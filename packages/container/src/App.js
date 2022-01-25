bcf4afb00302b007fc3daa2b30cfd66df6fc7b6e
cromero <cromero@amplify.com>
2022-01-25 15:55:37 -0500

    comment #3

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 //insert comment
 //insert another comment 
+
+
 export default () => {
     return <div><h1> Hello</h1>
     <hr />
